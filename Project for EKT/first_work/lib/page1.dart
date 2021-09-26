e7517570953e76278763399984659e0b8d5910c7
Niamul Hasan <48917799+niamul64@users.noreply.github.com>
2021-09-27 01:40:45 +0600

    appbar design complete

--- Project for EKT/first_work/lib/page1.dart
+++ Project for EKT/first_work/lib/page1.dart
@@ -15,32 +15,71 @@ class _Page1State extends State<Page1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30),
+            bottomRight: Radius.circular(30),
+          ),
+        ),
+
         centerTitle: false,
         backgroundColor: Colors.red.shade900,
-        title: Text('Explore'),
 
-        actions: const <Widget>[
-          Icon(
-            Icons.shopping_cart,
-            size: 32,
+        title: Text(
+            'Explore',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
           ),
-          Icon(
-            Icons.notifications_none,
-            size: 32,
+        ),
+
+        actions:  <Widget>[
+
+          Container(
+            margin: EdgeInsets.only(top:7),
+            child: RawMaterialButton(
+              onPressed: () {  },
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              fillColor: Colors.red.shade700,
+              child: Icon(Icons.shopping_cart,size: 25,),
+            ),
           ),
+          SizedBox(
+            width: 2,
+          ),
+          Container(
+            margin: EdgeInsets.only( top:7),
+            child: RawMaterialButton(
+              elevation: 6,
+              onPressed: () {  },
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              fillColor: Colors.red.shade700,
+              child: Icon(Icons.notifications_none,
+              size: 25,),
+            ),
+          ),
+
 
         ],
 
         bottom: AppBar(
+          toolbarHeight: 75,
           backgroundColor: Colors.red.shade900,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
+            ),
+          ),
 
           title:  Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15.0),
             ),
             color: Colors.white,
-            margin: EdgeInsets.fromLTRB(18,10, 18, 0),//left, top, right, bottom
+            margin: EdgeInsets.fromLTRB(18,8, 18, 15),//left, top, right, bottom
             child: ListTile(
               leading: Icon(
                 Icons.search,
